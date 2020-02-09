e021a720170e45d66026045716751a0327e53598
Chien-Chih Chen <Changetw0305@gmail.com>
2020-02-09 17:44:42 +0800

    Upload the first version codes.

--- ApacheIssueCrawler/ApacheIssueCrawler/ApacheIssueContent.cs
+++ ApacheIssueCrawler/ApacheIssueCrawler/ApacheIssueContent.cs
@@ -10,19 +10,45 @@ namespace ApacheIssueCrawler
         /// Assingee's Title.
         /// </summary>
         public string assingeeTitle { get; set; }
+
         /// <summary>
         /// Assingee's Content.
         /// </summary>
         public string assingeeContent { get; set; }
 
-
+        /// <summary>
+        /// Reporter's Title.
+        /// </summary>
         public string reportTitle { get; set; }
+
+        /// <summary>
+        /// Report's Content.
+        /// </summary>
         public string reporterContent { get; set; }
 
+        /// <summary>
+        /// Voters' Title.
+        /// </summary>
         public string votersTitle { get; set; }
+
+        /// <summary>
+        /// Voters' Content.
+        /// </summary>
         public string numberofVoters { get; set; }
 
+        /// <summary>
+        /// Watchers' Title.
+        /// </summary>
         public string watchersTitle { get; set; }
+
+        /// <summary>
+        /// Watehrs' Content.
+        /// </summary>
         public string numberofWatchers { get; set; }
+
+
+        public List<string> title = new List<string>();
+
+        public List<string> content = new List<string>();
     }
 }
--- ApacheIssueCrawler/ApacheIssueCrawler/Program.cs
+++ ApacheIssueCrawler/ApacheIssueCrawler/Program.cs
@@ -17,7 +17,64 @@ namespace ApacheIssueCrawler
 
             #region Step 01:get the raw data from the URL.
 
-            // People. 
+            #region Details
+            HtmlNodeCollection title = null;
+            HtmlNodeCollection content = null;
+            string outputValue = "";
+
+            // Type.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='issuedetails']/li[1]/div/strong");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='type-val']/text()");
+            addValuetoOutput(title, content, ref apacheIssueContent, true, true);
+
+            // Status.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='issuedetails']/li[2]/div/strong");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='status-val']/span");
+            addValuetoOutput(title, content, ref apacheIssueContent, true, true);
+
+            // Priority.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='issuedetails']/li[3]/div/strong");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='priority-val']/text()");
+            addValuetoOutput(title, content, ref apacheIssueContent, true, true);
+
+            // Resolution.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='issuedetails']/li[4]/div/strong");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='resolution-val']/text()");
+            addValuetoOutput(title, content, ref apacheIssueContent, true, true);
+
+            // Affects Version.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='issuedetails']/li[5]/div/strong");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='versions-field']/span");
+            addValuetoOutput(title, content, ref apacheIssueContent, true, true);
+
+            // Fix Version.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='issuedetails']/li[6]/div/strong");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='fixVersions-field']");
+            addValuetoOutput(title, content, ref apacheIssueContent, true, true);
+
+            // Component.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='issuedetails']/li[7]/div/strong");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='components-field']/a");
+            addValuetoOutput(title, content, ref apacheIssueContent, true, true);
+
+            // Labels.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='issuedetails']/li[8]/div/strong");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='labels-13028113-value']");
+            addValuetoOutput(title, content, ref apacheIssueContent, true, true);
+
+            // Patch Info.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='rowForcustomfield_12310041']/div/strong");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='customfield_12310041-field']/span");
+            addValuetoOutput(title, content, ref apacheIssueContent, true, true);
+
+            // Estimated Complexity.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='rowForcustomfield_12310060']/div/strong");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='customfield_12310060-val']");
+            addValuetoOutput(title, content, ref apacheIssueContent, true, true);
+
+            #endregion
+
+            #region People.
 
             // Assingee
             HtmlNodeCollection assgineeTitle = doc.DocumentNode.SelectNodes($"//*[@id='peopledetails']/li/dl[1]/dt");
@@ -34,7 +91,83 @@ namespace ApacheIssueCrawler
             // Watchers:
             HtmlNodeCollection watchersTitle = doc.DocumentNode.SelectNodes($"//*[@id='peoplemodule']/div[2]/ul[2]/li/dl[2]/dt");
             HtmlNodeCollection watchersContent = doc.DocumentNode.SelectNodes($"//*[@id='watcher-data']");
+            #endregion
 
+            #region Dates
+
+            // Create date.
+            // [TODO] get the epoch.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='datesmodule']/div[2]/ul/li/dl[1]/dt");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='created-val']/time");
+            foreach (var item in title)
+            {
+                outputValue += item.InnerText.ToString().Replace(System.Environment.NewLine, "").Trim().Replace(":","");
+            }
+            apacheIssueContent.title.Add(outputValue+=",");
+
+            outputValue = "";
+            foreach (var item in content)
+            {
+                outputValue += item.InnerText.ToString().Replace(System.Environment.NewLine, "").Trim();
+            }
+            apacheIssueContent.content.Add(outputValue += ",");
+
+            // Update date.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='datesmodule']/div[2]/ul/li/dl[2]/dt");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='updated-val']/time");
+
+            outputValue = "";
+            foreach (var item in title)
+            {
+                outputValue += item.InnerText.ToString().Replace(System.Environment.NewLine, "").Trim().Replace(":", "");
+            }
+            apacheIssueContent.title.Add(outputValue += ",");
+
+            outputValue = "";
+            foreach (var item in content)
+            {
+                outputValue += item.InnerText.ToString().Replace(System.Environment.NewLine, "").Trim();
+            }
+            apacheIssueContent.content.Add(outputValue += ",");
+
+            // Resolved date.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='datesmodule']/div[2]/ul/li/dl[3]/dt");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='resolutiondate-val']/time");
+
+            outputValue = "";
+            foreach (var item in title)
+            {
+                outputValue += item.InnerText.ToString().Replace(System.Environment.NewLine, "").Trim().Replace(":", "");
+            }
+            apacheIssueContent.title.Add(outputValue += ",");
+
+            outputValue = "";
+            foreach (var item in content)
+            {
+                outputValue += item.InnerText.ToString().Replace(System.Environment.NewLine, "").Trim();
+            }
+            apacheIssueContent.content.Add(outputValue += ",");
+
+            #endregion
+
+            #region Description & Comment
+
+            // Description.
+            title = doc.DocumentNode.SelectNodes($"//*[@id='descriptionmodule_heading']/h4");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='description-val']/div");
+            addValuetoOutput(title,content, ref apacheIssueContent, false, false);
+
+            // Comment.
+            /*
+            title = doc.DocumentNode.SelectNodes($"/html/body/div[1]/section/div[2]/div/div/div/div/div[2]/div/div/div/div[1]/div[5]/div[2]/div[1]/ul/li[2]/a/text()");
+            content = doc.DocumentNode.SelectNodes($"//*[@id='comment-15748543']/div[1]/div[1]/div[2]/text()");
+            */
+
+            // addValuetoOutput(title, content, ref apacheIssueContent);
+
+            #endregion
+
+            
 
             #endregion
 
@@ -115,6 +248,34 @@ namespace ApacheIssueCrawler
             #endregion
         }
 
+        private static void addValuetoOutput(HtmlNodeCollection title, HtmlNodeCollection content, ref ApacheIssueContent result, bool isRemoveContentInnerSpace, bool isReplaceInnerComma)
+        {
+           
+            string outputValue = "";
+            foreach (var item in title)
+            {
+                outputValue += item.InnerText.ToString().Replace(System.Environment.NewLine, "").Trim().Replace(":", "");
+            }
+          
+            result.title.Add(outputValue += ",");
+
+            outputValue = "";
+            foreach (var item in content)
+            {
+                outputValue += item.InnerText.ToString().Replace(System.Environment.NewLine, "").Trim();
+                if (isRemoveContentInnerSpace)
+                {
+                    outputValue = outputValue.Replace(",", "/");
+                }
+            }
+
+            if (isRemoveContentInnerSpace)
+            {
+                outputValue = outputValue.Replace(" ", "");
+            }
+            result.content.Add(outputValue += ",");
+        }
+
         /// <summary>
         /// Export conetnt to CSV file.
         /// </summary>
@@ -141,13 +302,25 @@ namespace ApacheIssueCrawler
             // Export the content to target file.
             using (var sm = new StreamWriter(fileFullPath))
             {
-                string title = data.assingeeTitle + "," + data.reportTitle + "," + data.votersTitle + "," + data.watchersTitle;
+                string title = data.assingeeTitle + "," + data.reportTitle + "," + data.votersTitle + "," + data.watchersTitle + ",";
                 title = title.Replace(":", "");
-                string content = data.assingeeContent + "," + data.reporterContent + "," + data.numberofVoters + "," + data.numberofWatchers;
+
+                foreach (var item in data.title)
+                {
+                    title += item;
+                }
+
+                string content = data.assingeeContent + "," + data.reporterContent + "," + data.numberofVoters + "," + data.numberofWatchers + ",";
+
+                foreach (var item in data.content)
+                {
+                    content += item;
+                }
+
                 sm.WriteLineAsync(title);
                 sm.WriteLineAsync(content);
-                sm.Flush();
-                sm.Close();
+                //sm.Flush();
+                //sm.Close();
             }
         }
     }
